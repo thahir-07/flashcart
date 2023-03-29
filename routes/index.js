13d1b68628acb5fc104cc85a85aca5ac84454f07
thahir-07 <abuthahircoorg07@gmail.com>
2023-03-29 10:05:48 +0530

    admin and user pannel seperated

--- routes/index.js
+++ routes/index.js
@@ -7,115 +7,132 @@ router.get('/', function (req, res, next) {
     name: "iphone 14",
     category: "smart phone",
     description: " latest pill notch display with face id and more features",
-    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/3/l/x/-original-imaghmtcmxwgypsk.jpeg?q=90"
+    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
   },
   {
     name: "samsung s21",
     category: "smart phone",
     description: " latest good looking design with 100x zoom rear camera",
-    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
+    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/3/l/x/-original-imaghmtcmxwgypsk.jpeg?q=90"
   },{
+    name: "moto g fushion",
+    category: "smart phone",
+    description: " latest good looking design with curve display",
+    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/a/8/a/-original-imagbwx4ahgqxgg9.jpeg?q=90"
+  },{
+    name: "iphone 14",
+    category: "smart phone",
+    description: " latest pill notch display with face id and more features",
+    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
+  },
+  {
     name: "samsung s21",
     category: "smart phone",
     description: " latest good looking design with 100x zoom rear camera",
-    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
+    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/3/l/x/-original-imaghmtcmxwgypsk.jpeg?q=90"
   },{
+    name: "moto g fushion",
+    category: "smart phone",
+    description: " latest good looking design with curve display",
+    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/a/8/a/-original-imagbwx4ahgqxgg9.jpeg?q=90"
+  },{
+    name: "iphone 14",
+    category: "smart phone",
+    description: " latest pill notch display with face id and more features",
+    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
+  },
+  {
     name: "samsung s21",
     category: "smart phone",
     description: " latest good looking design with 100x zoom rear camera",
-    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
+    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/3/l/x/-original-imaghmtcmxwgypsk.jpeg?q=90"
   },{
+    name: "moto g fushion",
+    category: "smart phone",
+    description: " latest good looking design with curve display",
+    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/a/8/a/-original-imagbwx4ahgqxgg9.jpeg?q=90"
+  },{
+    name: "iphone 14",
+    category: "smart phone",
+    description: " latest pill notch display with face id and more features",
+    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
+  },
+  {
     name: "samsung s21",
     category: "smart phone",
     description: " latest good looking design with 100x zoom rear camera",
-    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
+    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/3/l/x/-original-imaghmtcmxwgypsk.jpeg?q=90"
   },{
+    name: "moto g fushion",
+    category: "smart phone",
+    description: " latest good looking design with curve display",
+    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/a/8/a/-original-imagbwx4ahgqxgg9.jpeg?q=90"
+  },{
+    name: "iphone 14",
+    category: "smart phone",
+    description: " latest pill notch display with face id and more features",
+    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
+  },
+  {
     name: "samsung s21",
     category: "smart phone",
     description: " latest good looking design with 100x zoom rear camera",
-    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
+    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/3/l/x/-original-imaghmtcmxwgypsk.jpeg?q=90"
   },{
+    name: "moto g fushion",
+    category: "smart phone",
+    description: " latest good looking design with curve display",
+    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/a/8/a/-original-imagbwx4ahgqxgg9.jpeg?q=90"
+  },{
+    name: "iphone 14",
+    category: "smart phone",
+    description: " latest pill notch display with face id and more features",
+    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
+  },
+  {
     name: "samsung s21",
     category: "smart phone",
     description: " latest good looking design with 100x zoom rear camera",
-    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
+    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/3/l/x/-original-imaghmtcmxwgypsk.jpeg?q=90"
   },{
+    name: "moto g fushion",
+    category: "smart phone",
+    description: " latest good looking design with curve display",
+    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/a/8/a/-original-imagbwx4ahgqxgg9.jpeg?q=90"
+  },{
+    name: "iphone 14",
+    category: "smart phone",
+    description: " latest pill notch display with face id and more features",
+    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
+  },
+  {
     name: "samsung s21",
     category: "smart phone",
     description: " latest good looking design with 100x zoom rear camera",
-    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
+    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/3/l/x/-original-imaghmtcmxwgypsk.jpeg?q=90"
   },{
+    name: "moto g fushion",
+    category: "smart phone",
+    description: " latest good looking design with curve display",
+    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/a/8/a/-original-imagbwx4ahgqxgg9.jpeg?q=90"
+  },{
+    name: "iphone 14",
+    category: "smart phone",
+    description: " latest pill notch display with face id and more features",
+    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
+  },
+  {
     name: "samsung s21",
     category: "smart phone",
     description: " latest good looking design with 100x zoom rear camera",
-    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
+    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/3/l/x/-original-imaghmtcmxwgypsk.jpeg?q=90"
   },{
-    name: "samsung s21",
+    name: "moto g fushion",
     category: "smart phone",
-    description: " latest good looking design with 100x zoom rear camera",
-    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
-  },{
-    name: "samsung s21",
-    category: "smart phone",
-    description: " latest good looking design with 100x zoom rear camera",
-    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
-  },{
-    name: "samsung s21",
-    category: "smart phone",
-    description: " latest good looking design with 100x zoom rear camera",
-    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
-  },{
-    name: "samsung s21",
-    category: "smart phone",
-    description: " latest good looking design with 100x zoom rear camera",
-    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
-  },{
-    name: "samsung s21",
-    category: "smart phone",
-    description: " latest good looking design with 100x zoom rear camera",
-    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
-  },{
-    name: "samsung s21",
-    category: "smart phone",
-    description: " latest good looking design with 100x zoom rear camera",
-    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
-  },{
-    name: "samsung s21",
-    category: "smart phone",
-    description: " latest good looking design with 100x zoom rear camera",
-    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
-  },{
-    name: "samsung s21",
-    category: "smart phone",
-    description: " latest good looking design with 100x zoom rear camera",
-    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
-  },{
-    name: "samsung s21",
-    category: "smart phone",
-    description: " latest good looking design with 100x zoom rear camera",
-    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
-  },{
-    name: "samsung s21",
-    category: "smart phone",
-    description: " latest good looking design with 100x zoom rear camera",
-    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
-  },{
-    name: "samsung s21",
-    category: "smart phone",
-    description: " latest good looking design with 100x zoom rear camera",
-    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
-  },{
-    name: "samsung s21",
-    category: "smart phone",
-    description: " latest good looking design with 100x zoom rear camera",
-    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
-  },{
-    name: "samsung s21",
-    category: "smart phone",
-    description: " latest good looking design with 100x zoom rear camera",
-    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/m/j/j/-original-imagnvvwvzyhfvx9.jpeg?q=90"
+    description: " latest good looking design with curve display",
+    image:"https://rukminim1.flixcart.com/image/300/300/xif0q/mobile/a/8/a/-original-imagbwx4ahgqxgg9.jpeg?q=90"
   } ]
-  res.render('index', {products});
+  res.render('index', {products,admin:true});
 });
 
 module.exports = router;
