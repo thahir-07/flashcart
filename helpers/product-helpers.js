8cdc450a85478334b666947632f659ca4e6c5e03
thahir-07 <abuthahircoorg07@gmail.com>
2023-04-10 00:22:36 +0530

    user cart page added and using aggregate products data fetched

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -53,7 +53,18 @@ module.exports={
             let userCart=await db.get().collection(collections.CART_COLLECTION).findOne({user:new ObjectId(userId)})
             
             if(userCart){
-                console.log(userCart)
+                db.get().collection(collections.CART_COLLECTION).updateOne(
+                    {user:new ObjectId(userId)},
+                    {
+                        $push:{products:new ObjectId(proId)}
+                    }
+                    ).then((response)=>{
+                        console.log("cart updated")
+                        resolve(response)
+
+                    })
+               
+                
 
             }
             else{
@@ -62,6 +73,7 @@ module.exports={
                     products:[new ObjectId(proId)]
                 }
                 db.get().collection(collections.CART_COLLECTION).insertOne(cardObj).then((response)=>{
+                    console.log("user cart created")
                    
                     resolve(response)
                 })
@@ -72,6 +84,54 @@ module.exports={
 
         
 
+    },/*getCartProducts:(userId)=>{
+        return new Promise(async(resolve,reject)=>{
+       let productsValues=await db.get().collection(collections.CART_COLLECTION).findOne({user:new ObjectId(userId)},{_id:0})
+       console.log("products are.......")
+       console.log(productsValues.products[0])
+       let productDetails=[]
+       let length=productsValues.products.length
+       for(i=0;i<length;i++){
+
+         productDetails.push(await db.get().collection(collections.PRODUCT_COLLECTION).findOne({_id:new ObjectId(productsValues.products[i])}))
+       }
+       console.log("after array iteration")
+       console.log(productDetails)
+       resolve(productDetails)
+       
+    })
+   
+}*/
+//above code is written without aggregate and code working properly
+getCartProducts:((userId)=>{
+    return new Promise(async(resolve,reject)=>{
+      let cartItems=await db.get().collection(collections.CART_COLLECTION).aggregate([
+            {
+                $match:{user:new ObjectId(userId)}
+            },
+            {
+                $lookup:{
+                    from:collections.PRODUCT_COLLECTION,
+                    let:{productList:'$products'},
+                    pipeline:[
+                        {
+                            $match:{
+                                $expr:{
+                                    $in:['$_id','$$productList']
+                                }
+                            }
+                        }
+                    ],
+                    as:'cartItems'
+
+                }
+            }
+        ]).toArray()
+        
+        resolve(cartItems[0].cartItems)
     }
    
+    )
+
+})
 }
--- routes/user.js
+++ routes/user.js
@@ -3,96 +3,110 @@ var router = express.Router();
 var db = require('../config/connection')
 var collections = require('../config/collections')
 var producthelper = require('../helpers/product-helpers')
-var userhelpers=require('../helpers/user-helpers')
-let user=null
+var userhelpers = require('../helpers/user-helpers')
+let user = null
 
 
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  user=req.session.user
-  
+  user = req.session.user
+
   producthelper.getAllProduct().then((product) => {
-    
-    res.render('user/view-products', { product, admin: false ,user})
+
+    res.render('user/view-products', { product, admin: false, user })
   })
 })
 
-router.get('/login',function(req,res){
-  if(req.session.loggedIn)
-  {
+router.get('/login', function (req, res) {
+  if (req.session.loggedIn) {
     res.redirect('/')
-  
-  }else{
-    
-    res.render('user/user-login',{logerr:req.session.loginErr})
-    req.session.loginErr=false
+
+  } else {
+
+    res.render('user/user-login', { logerr: req.session.loginErr })
+    req.session.loginErr = false
 
 
   }
 })
-  
 
-router.get('/signup',function(req,res){
+
+router.get('/signup', function (req, res) {
   res.render('user/user-signup')
 })
-router.post('/signup',function(req,res){
-  userhelpers.doSignup(req.body).then((response)=>{
-    req.session.loggedIn=true
-    req.session.user=response
-    user=req.session.user
+router.post('/signup', function (req, res) {
+  userhelpers.doSignup(req.body).then((response) => {
+    req.session.loggedIn = true
+    req.session.user = response
+    user = req.session.user
     res.redirect('/login')
   })
 })
-router.post('/login',function(req,res){
+router.post('/login', function (req, res) {
 
-  
-  userhelpers.doLogin(req.body).then((response)=>{
-    if(response.status){
-      req.session.loggedIn=true
-      req.session.user=response.user
+
+  userhelpers.doLogin(req.body).then((response) => {
+    if (response.status) {
+      req.session.loggedIn = true
+      req.session.user = response.user
       res.redirect('/')
-    }else{
-      req.session.loginErr="Invalid Email or Password"
-      
+    } else {
+      req.session.loginErr = "Invalid Email or Password"
+
       res.redirect('/login')
     }
 
   })
 })
-router.get('/logout',function(req,res){
+router.get('/logout', function (req, res) {
   req.session.destroy()
   res.redirect('/')
 })
-router.get('/cart',verifyLogin,function(req,res){
-
-})
-
-function verifyLogin(req,res,next){
-  let user=req.session.user
-  if(req.session.loggedIn)
-     res.render('user/user-cart',{user})
-  else
-  res.render('user/user-login')
-next()
-}
-router.get('/addtocart/:id',verifyLogin,(req,res)=>{
-  var id=req.params.id
-  if(req.session.loggedIn){
-    producthelper.addToCart(id,req.session.user._id).then((resolve)=>{
-      res.render('user/add-to-cart')
-
+router.get('/cart', function (req, res) {
+  if (req.session.user) {
+    producthelper.getCartProducts(req.session.user._id).then((response) => {
+      let products = response
+      console.log(products)
+      res.render('user/user-cart',{products,user})
     })
-    
 
-  } 
-  else{
+  }else{
     res.redirect('/login')
+
   }
- 
+}
+)
+
+function verifyLogin(req, res, next) {
+  let user = req.session.user
+  if (req.session.loggedIn)
+    res.render('user/user-cart', { user })
+  else
+    res.render('user/user-login')
+  next()
+}
+router.get('/addtocart/:id',  (req, res) => {
+  var id = req.params.id
+  if (req.session.loggedIn) {
+    producthelper.addToCart(id, req.session.user._id).then((response) => {
+      res.redirect('/')
 
 
-})
+      })
+
+
+    }else{
+      res.redirect('/login')
+    }
+
+
+  }
+  
+
+
+
+)
 module.exports = router
 
 
